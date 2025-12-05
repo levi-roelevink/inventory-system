278afb7e63df1d6e3ef87f72081e3f8f89fbe32a
Levi Roelevink <538253@student.saxion.nl>
2025-12-05 12:36:57 +0100

    Menu selection for inventory management

--- main.cpp
+++ main.cpp
@@ -1,10 +1,67 @@
 #include <cstddef>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
 
 const string PATH = "inventory.txt";
+
+const int LIST_INVENTORY = 1;
+const int ADD_PRODUCT = 2;
+const int REMOVE_PRODUCT = 3;
+const int EXIT = 4;
+const string PRINT_MENU =
+    "**********\n(1) List inventory\n(2) Add "
+    "product\n(3) Remove product\n(4) Exit program\n**********\n";
+
+int selectBetweenBounds(int lowerBound, int upperBound) {
+  int result;
+
+  cout << "Select an option: ";
+  cin >> result;
+  // TODO: What if input is not an integer
+  while (result < lowerBound || result > upperBound) {
+    cout << "\nOption must be between " << lowerBound << " and " << upperBound
+         << ".\nSelect an option: ";
+    cin >> result;
+  }
+
+  cout << "\n";
+  return result;
+}
+
+void listInventory() { 
+  cout << "List inventory" << endl; 
+}
+
+void addProduct() { cout << "Add product" << endl; }
+
+void removeProduct() { cout << "Remove product" << endl; }
+
+// Print menu options
+// Prompt user for menu selection
+// Call selected functionality
+int menuSelection() {
+  while (true) {
+    cout << PRINT_MENU;
+    int input = selectBetweenBounds(1, 4);
+
+    switch (input) {
+    case LIST_INVENTORY:
+      listInventory();
+      break;
+    case ADD_PRODUCT:
+      addProduct();
+      break;
+    case REMOVE_PRODUCT:
+      removeProduct();
+      break;
+    case EXIT:
+      return 0;
+    }
+  }
+}
 
 fstream openFile(string path) {
   // Open file for reading and writing
@@ -40,7 +97,6 @@ string promptUser() {
   cout << "Product name: ";
 
   string input;
-
   cin >> input;
   // TODO: What if there's a whitespace character?
   // Also try adding multiple items to see if they're formatted correctly in the
@@ -63,13 +119,15 @@ int main() {
   file << "Mango\n";
   file << "Kiwi\n";
 
-  addItem(&file);
-
-  // Reset file position pointer before switching from write to read
-  file.seekg(0);
-  printFileContents(&file);
+  int menuSelector = menuSelection();
 
   file.close();
-
+  cout << "Goodbye!" << endl;
   return 0;
 }
+
+// addItem(&file);
+
+// Reset file position pointer before switching from write to read
+// file.seekg(0);
+// printFileContents(&file);
